d5a31862014001c8f683c85b64e77f5f90078773
Keenan Cassidy <keenan.cassidy@Keenans-MacBook-Pro.local>
2015-08-30 17:30:37 +0100

    KYO: added easing

--- TextFieldEffects/TextFieldEffects/KyoTextField.swift
+++ TextFieldEffects/TextFieldEffects/KyoTextField.swift
@@ -22,16 +22,19 @@ import UIKit
     @IBInspectable var textFieldBackgroundColor: UIColor?
     
     private var textFieldBackgroundView = UIView()
-    private var inActiveColor = UIColor()
+    private var colorViewTopConstraint = NSLayoutConstraint()
+    private var colorViewHeightConstraint = NSLayoutConstraint()
+    private var colorViewLeftConstraint = NSLayoutConstraint()
+    private var colorViewRightConstraint = NSLayoutConstraint()
+    
+    let colorView = UIView()
     
     override public func drawViewsForRect(rect: CGRect) {
         updateBorder()
         updatePlaceholder()
-        
-        inActiveColor = (superview?.backgroundColor)!
-        
         addSubview(placeholderLabel)
         addTextFieldBackgroundView()
+        addColorView()
     }
     
     private func updateBorder() {
@@ -55,6 +58,38 @@ import UIKit
         
         addSubview(textFieldBackgroundView)
         sendSubviewToBack(textFieldBackgroundView)
+        addTextFieldBackgroundViewConstraints()
+    }
+    
+    private func addTextFieldBackgroundViewConstraints() {
+        textFieldBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let topConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: textFieldBackgroundView, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: textFieldBackgroundView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: textFieldBackgroundView.frame.size.height)
+        let leftConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: textFieldBackgroundView, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 0)
+        let rightConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: textFieldBackgroundView, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: 0)
+        
+        addConstraints([topConstraint, heightConstraint, leftConstraint, rightConstraint])
+    }
+    
+    private func addColorView() {
+        colorView.frame = superview!.convertRect(textFieldBackgroundView.frame, fromView:self)
+        colorView.layer.cornerRadius = cornerRadius
+        colorView.backgroundColor = activeColor
+        superview?.addSubview(colorView)
+        superview?.bringSubviewToFront(self)
+        addColorViewConstraints()
+    }
+    
+    private func addColorViewConstraints() {
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        colorViewTopConstraint = NSLayoutConstraint(item: colorView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: superview!, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: colorView.frame.origin.y)
+        colorViewHeightConstraint = NSLayoutConstraint(item: colorView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: colorView.frame.size.height)
+        colorViewLeftConstraint = NSLayoutConstraint(item: colorView, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: superview!, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: colorView.frame.origin.x)
+        colorViewRightConstraint = NSLayoutConstraint(item: superview!, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: colorView, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: colorView.frame.origin.x)
+        
+        superview!.addConstraints([colorViewTopConstraint, colorViewHeightConstraint, colorViewLeftConstraint, colorViewRightConstraint])
     }
     
     func tap() {
@@ -68,17 +103,33 @@ import UIKit
     }
 
     public override func animateViewsForTextEntry() {
-        UIView.animateWithDuration(fadeSpeed, animations: { () -> Void in
-            self.superview?.backgroundColor = self.activeColor
+        colorViewTopConstraint.constant = 0
+        colorViewHeightConstraint.constant = (superview?.frame.size.height)!
+        colorViewLeftConstraint.constant = -cornerRadius
+        colorViewRightConstraint.constant = -cornerRadius
+        
+        superview?.setNeedsUpdateConstraints()
+        
+        UIView.animateWithDuration(fadeSpeed, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+            self.superview?.layoutIfNeeded()
             self.placeholderLabel.textColor = self.activeTextColor
-        })
+            }, completion: nil)
     }
     
     public override func animateViewsForTextDisplay() {
-        UIView.animateWithDuration(fadeSpeed, animations: { () -> Void in
-            self.superview?.backgroundColor = self.inActiveColor
+        let textFieldBackgroundViewLocationInSuperView = self.superview!.convertRect(self.textFieldBackgroundView.frame, fromView:self)
+        
+        colorViewTopConstraint.constant = textFieldBackgroundViewLocationInSuperView.origin.y
+        colorViewHeightConstraint.constant = textFieldBackgroundViewLocationInSuperView.size.height
+        colorViewLeftConstraint.constant = textFieldBackgroundViewLocationInSuperView.origin.x
+        colorViewRightConstraint.constant = textFieldBackgroundViewLocationInSuperView.origin.x
+
+        superview?.setNeedsUpdateConstraints()
+        
+        UIView.animateWithDuration(fadeSpeed, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            self.superview?.layoutIfNeeded()
             self.placeholderLabel.textColor = self.textColor
-        })
+            }, completion: nil)
     }
     
     override public func editingRectForBounds(bounds: CGRect) -> CGRect {
